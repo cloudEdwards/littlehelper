7a39474ac95bd363b000bf9f26ecdea79bebee9c
Cloud Edwards <kumo.cloud@gmail.com>
2015-03-22 15:45:43 -0700

    Set Paypal API to LIVE credentials

--- app/tools/Billing/PaypalBilling.php
+++ app/tools/Billing/PaypalBilling.php
@@ -46,7 +46,7 @@ class PaypalBilling implements BillingInterface
 
 		$data_string = json_encode($dataLoad);                                                                                   
 		 
-		$ch = curl_init('https://svcs.sandbox.paypal.com/AdaptivePayments/Pay');                                                                      
+		$ch = curl_init('https://svcs.paypal.com/AdaptivePayments/Pay');                                                                      
 		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
 		curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
@@ -55,11 +55,11 @@ class PaypalBilling implements BillingInterface
 			'Content-Type: application/json',                                                                                
 			'Content-Length: '. strlen($data_string),
 			// Sandbox API credentials for the API Caller account                                                       
-			'X-PAYPAL-SECURITY-USERID : kumo.cloud_api1.gmail.com',
-			'X-PAYPAL-SECURITY-PASSWORD : E887SPW9F5XZ9ZZ8',
-			'X-PAYPAL-SECURITY-SIGNATURE : AFcWxV21C7fd0v3bYYYRCpSSRl31AHoNvoOZH-4m-9gpLxgk7Ab0MYTJ',
+			'X-PAYPAL-SECURITY-USERID : littlehelpersaws_api1.gmail.com',
+			'X-PAYPAL-SECURITY-PASSWORD : ZKJZZC35WM46QCAF',
+			'X-PAYPAL-SECURITY-SIGNATURE : A7ErME.S7jXxqqfj1NWlnWs.56bKAIrMK0mJAalRt4taiBWH6uD0Gwy2',
 			// Global Sandbox Application ID
-			'X-PAYPAL-APPLICATION-ID : APP-80W284485P519543T',
+			'X-PAYPAL-APPLICATION-ID : APP-0F9624499K4942523',
 			// Input and output formats
 			'X-PAYPAL-REQUEST-DATA-FORMAT : JSON',
 			'X-PAYPAL-RESPONSE-DATA-FORMAT : JSON ')                                                                      
