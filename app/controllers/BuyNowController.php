a7f15cde081e3229947a093159c4f55e40925af4
Cloud Edwards <kumo.cloud@gmail.com>
2015-03-22 11:26:35 -0700

    Paypal debugging trying to get it going

--- app/controllers/BuyNowController.php
+++ app/controllers/BuyNowController.php
@@ -166,44 +166,6 @@ class BuyNowController extends \BaseController {
 		$url = 'https://www.sandbox.paypal.com/cgi-bin/webscr?cmd=_ap-payment&paykey='
 				.$result['payKey'];
 
-		//  Send Invoicing correspondance
-		$order = BillingLog::findOrFail($input['hash'])['attributes'];
-		$price = Prices::findOrFail(1);
-
-		$confirmed_bill = [
-				'data'=>$input,
-				'order'=>$order,
-				'price'=>$result['price'],
-				'total'=>$result['total']
-				];
-		
-		$order = $confirmed_bill['order'];
-		$email = $order['email'];
-		$msg = "Hello ";
-
-		// Send Customer Invoice
-		Mail::send('emails.invoice', ['invoice'=>$confirmed_bill], function($message) use ($email)
-			{
-			    $message->to($email, 'Cloud')->subject('Thank You!');
-			});
-		// Send Order to Manufacturer
-		Mail::send('emails.order', ['invoice'=>$confirmed_bill], function($message)  use ($email)
-			{
-			    $message->to($email, 'Cloud')->subject('New Order!');
-			});
-
-		// Send Text Message
-
-		$account_sid = 'ACb6f468752fecc4eb3ebf70648b71e347'; 
-		$auth_token = '4575f860099381b42b4a60bcac14f84f'; 
-		$client = new Services_Twilio($account_sid, $auth_token); 
-		 
-		$client->account->messages->create(array( 
-			'To' => "12503543711", 
-			'From' => "+13345641913", 
-			'Body' => "You have a New Order from LittleHelper.Chainsaw, check your email.",   
-		));
-
 		return Redirect::to($url);
 		//return View::make('buy/paypal')->withUrl($url);
 	}
