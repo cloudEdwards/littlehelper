6009e135772ccebbc3a58e3d4ec6d79cd58de1a9
Cloud Edwards <kumo.cloud@gmail.com>
2015-03-22 10:58:33 -0700

    Changed email variables

--- app/views/emails/invoice.blade.php
+++ app/views/emails/invoice.blade.php
@@ -1,23 +1,21 @@
 
 <?php  
-	$data = $invoice['data'];
-	$order = $invoice['order'];
-	$price = $invoice['price'];
-	$total = $invoice['total'];
+	$order = $invoice;
+	$total = $invoice['amount'];
  ?>
-
- <style type="text/css"> table,tr,th,td {border:solid;} </style>
+ <style type="text/css"> table,tr,th,td {border:groove;} </style>
 
 <h2>Thank you for your business!</h2>
 
-<p>We will send your order as soon as we confirm the transaction on our end.</p>
+<p>I will send your order as soon possible. I hope enjoy your chainsaw for generations, like I have.</p>
+<p>~ Angel</p>
 
-<h3><small>Invoice Code: {{$order['id']}}</small></h3>
-<h3>Price: ${{number_format($price/100,2)}}</h3>
+
 <h3>Quantity: {{$order['quantity']}}</h3>
-<h3>Sub Total: ${{number_format($price/100 * $order['quantity'],2)}}</h3>
+
 <h3>Shipping: ${{number_format($order['shipping'],2)}}</h3>
-<h3><strong>Total: ${{number_format($total/100,2)}}</strong></h3>
+
+<h3><strong>Total: ${{number_format($total,2)}}</strong></h3>
 <br>
 <h3><small>Name: {{$order['name']}}</small></h3>
 <h3><small>Phone: {{$order['phonenumber']}}</small></h3>
@@ -29,4 +27,4 @@
 <h3><small>Postal/Zip Code: {{$order['postalcode']}}</small></h3>
 
 
-
+<h3><small>Invoice Code: {{$order['id']}}</small></h3>
--- app/views/emails/order.blade.php
+++ app/views/emails/order.blade.php
@@ -1,11 +1,9 @@
 
 <?php  
-	$data = $invoice['data'];
-	$order = $invoice['order'];
-	$price = $invoice['price'];
-	$total = $invoice['total'];
+	$order = $invoice;
+	$total = $invoice['amount'];
  ?>
- <style type="text/css"> table,tr,th,td {border:solid;} </style>
+ <style type="text/css"> table,tr,th,td {border:groove;} </style>
 
 <h2>A Customer Placed an Order!</h2>
 
@@ -13,11 +11,12 @@
 
 
 <h3><small>Invoice Code: {{$order['id']}}</small></h3>
-<h3>Price: ${{number_format($price/100,2)}}</h3>
+
 <h3>Quantity: {{$order['quantity']}}</h3>
-<h3>Sub Total: ${{number_format($price/100 * $order['quantity'],2)}}</h3>
+
 <h3>Shipping: ${{number_format($order['shipping'],2)}}</h3>
-<h3><strong>Total: ${{number_format($total/100,2)}}</strong></h3>
+
+<h3><strong>Total: ${{number_format($total,2)}}</strong></h3>
 <br>
 <h3><small>Name: {{$order['name']}}</small></h3>
 <h3><small>Phone: {{$order['phonenumber']}}</small></h3>
