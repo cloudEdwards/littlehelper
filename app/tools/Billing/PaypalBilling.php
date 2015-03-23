1478fb1356c80d395ab2d0500a0644ea1ae29c63
Cloud Edwards <kumo.cloud@gmail.com>
2015-03-23 11:53:20 -0700

    Refactored and commented code

--- app/tools/Billing/PaypalBilling.php
+++ app/tools/Billing/PaypalBilling.php
@@ -55,11 +55,11 @@ class PaypalBilling implements BillingInterface
 			'Content-Type: application/json',                                                                                
 			'Content-Length: '. strlen($data_string),
 			// Sandbox API credentials for the API Caller account                                                       
-			'X-PAYPAL-SECURITY-USERID : littlehelpersaws_api1.gmail.com',
-			'X-PAYPAL-SECURITY-PASSWORD : ZKJZZC35WM46QCAF',
-			'X-PAYPAL-SECURITY-SIGNATURE : A7ErME.S7jXxqqfj1NWlnWs.56bKAIrMK0mJAalRt4taiBWH6uD0Gwy2',
+			'X-PAYPAL-SECURITY-USERID : '.$_ENV['PP_USER_ID'],
+			'X-PAYPAL-SECURITY-PASSWORD : '.$_ENV['PP_PASSWORD'],
+			'X-PAYPAL-SECURITY-SIGNATURE : '.$_ENV['PP_SIGNATURE'],
 			// Global Sandbox Application ID
-			'X-PAYPAL-APPLICATION-ID : APP-1NM59315J2128684U',
+			'X-PAYPAL-APPLICATION-ID : '.$_ENV['APP_ID'],
 			// Input and output formats
 			'X-PAYPAL-REQUEST-DATA-FORMAT : JSON',
 			'X-PAYPAL-RESPONSE-DATA-FORMAT : JSON ')                                                                      
